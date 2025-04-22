2c38627b292f06cee9e22dcc8f551d7eab59ed22
Nuha Alansari <Nuha.Alansari@ibm.com>
2025-04-22 15:50:13 +0300

    added

--- app.py
+++ app.py
@@ -18,4 +18,4 @@ def add_user():
     return jsonify(new_item), 201  
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8000)))
